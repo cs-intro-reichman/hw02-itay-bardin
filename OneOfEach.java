f8ac2a3d63ba29382f3a5f3b648a2fe1b0e803bc
Itay <itay.bardin@post.runi.ac.il>
2023-12-27 13:02:46 +0200

    Added OneOfEach

--- OneOfEach.java
+++ OneOfEach.java
@@ -5,6 +5,22 @@
  */
 public class OneOfEach {
 	public static void main (String[] args) {
-		//// Put your code here
+		double rand = Math.random();
+		String str = "";
+		int boys = 0;
+		int girls = 0;
+			while(boys <1 || girls <1){
+				if(rand < 0.5){
+					boys++;
+					str = str + "b ";
+				} 
+				else{
+					girls++;
+					str = str + "g ";
+				}
+			rand = Math.random();
+			}
+		System.out.println(str);
+		System.out.println("You Made it... and now you have " + (girls + boys) + " children");
 	}
 }
